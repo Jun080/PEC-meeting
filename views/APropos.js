c3c3d642cfad0d2a20a89949b0d3c44d346d70fd
Dassonville Morgane <morganedassonville08@gmail.com>
2025-07-20 08:42:04 +0200

    finish about page

--- views/APropos.js
+++ views/APropos.js
@@ -307,6 +307,264 @@ const APropos = function () {
                 ]
               }
             ]
+          },
+          {
+            tag: "section",
+            attributes: [["class", "apropos-simplicite"]],
+            children: [
+              {
+                tag: "div",
+                attributes: [["class", "container-small"]],
+                children: [
+                  {
+                    tag: "h2",
+                    attributes: [["class", "h2-big simplicite-title"]],
+                    children: ["Une plateforme pensée pour la simplicité"]
+                  },
+                  {
+                    tag: "div",
+                    attributes: [["class", "etapes-container"]],
+                    children: [
+                      {
+                        tag: "div",
+                        attributes: [["class", "etape etape-1"]],
+                        children: [
+                          {
+                            tag: "div",
+                            attributes: [["class", "etape-content"]],
+                            children: [
+                              {
+                                tag: "h3",
+                                attributes: [["class", "h2"]],
+                                children: [
+                                  "Inscription ",
+                                  {
+                                    tag: "strong",
+                                    children: ["rapide"]
+                                  },
+                                  " et gratuite"
+                                ]
+                              },
+                              {
+                                tag: "button",
+                                attributes: [["class", "bouton-primary-1"]],
+                                children: ["Je m'inscris"]
+                              }
+                            ]
+                          },
+                          {
+                            tag: "div",
+                            attributes: [["class", "etape-image"]],
+                            children: [
+                              {
+                                tag: "img",
+                                attributes: [
+                                  ["src", "../Assets/images/inscription-rapide.webp"],
+                                  ["alt", "Inscription rapide et gratuite"]
+                                ]
+                              }
+                            ]
+                          }
+                        ]
+                      },
+                      {
+                        tag: "div",
+                        attributes: [["class", "etape etape-2"]],
+                        children: [
+                          {
+                            tag: "div",
+                            attributes: [["class", "etape-content"]],
+                            children: [
+                              {
+                                tag: "h3",
+                                attributes: [["class", "h2"]],
+                                children: [
+                                  "Création d'événement ",
+                                  {
+                                    tag: "strong",
+                                    children: ["en quelques clics"]
+                                  }
+                                ]
+                              },
+                              {
+                                tag: "button",
+                                attributes: [["class", "bouton-primary-1"]],
+                                children: ["Créer mon événement"]
+                              }
+                            ]
+                          },
+                          {
+                            tag: "div",
+                            attributes: [["class", "etape-image"]],
+                            children: [
+                              {
+                                tag: "img",
+                                attributes: [
+                                  ["src", "../Assets/images/creation-evenement.webp"],
+                                  ["alt", "Création d'événement en quelques clics"]
+                                ]
+                              }
+                            ]
+                          }
+                        ]
+                      },
+                      {
+                        tag: "div",
+                        attributes: [["class", "etape etape-3"]],
+                        children: [
+                          {
+                            tag: "div",
+                            attributes: [["class", "etape-content"]],
+                            children: [
+                              {
+                                tag: "h3",
+                                attributes: [["class", "h2"]],
+                                children: [
+                                  "Exploration ",
+                                  {
+                                    tag: "strong",
+                                    children: ["intuitive"]
+                                  },
+                                  " des communautés et événements"
+                                ]
+                              },
+                              {
+                                tag: "button",
+                                attributes: [["class", "bouton-primary-1"]],
+                                children: ["Je commence !"]
+                              }
+                            ]
+                          },
+                          {
+                            tag: "div",
+                            attributes: [["class", "etape-image"]],
+                            children: [
+                              {
+                                tag: "img",
+                                attributes: [
+                                  ["src", "../Assets/images/exploration-intuitive.webp"],
+                                  ["alt", "Exploration intuitive des communautés et événements"]
+                                ]
+                              }
+                            ]
+                          }
+                        ]
+                      },
+                      {
+                        tag: "div",
+                        attributes: [["class", "etape etape-4"]],
+                        children: [
+                          {
+                            tag: "div",
+                            attributes: [["class", "etape-content"]],
+                            children: [
+                              {
+                                tag: "h3",
+                                attributes: [["class", "h2"]],
+                                children: [
+                                  "Notifications ",
+                                  {
+                                    tag: "strong",
+                                    children: ["en temps réel"]
+                                  },
+                                  " pour ne rien rater"
+                                ]
+                              },
+                              {
+                                tag: "button",
+                                attributes: [["class", "bouton-primary-1"]],
+                                children: ["Activer les notifications"]
+                              }
+                            ]
+                          },
+                          {
+                            tag: "div",
+                            attributes: [["class", "etape-image"]],
+                            children: [
+                              {
+                                tag: "img",
+                                attributes: [
+                                  ["src", "../Assets/images/notification-temps-reel.webp"],
+                                  ["alt", "Notifications en temps réel pour ne rien rater"]
+                                ]
+                              }
+                            ]
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            tag: "section",
+            attributes: [["class", "apropos-pourquoi"]],
+            children: [
+              {
+                tag: "div",
+                attributes: [["class", "container"]],
+                children: [
+                  {
+                    tag: "h2",
+                    attributes: [["class", "h2-big"]],
+                    children: ["Pourquoi Meetup Connect a été créé ?"]
+                  },
+                  {
+                    tag: "div",
+                    attributes: [["class", "text-container"]],
+                    children: [
+                      {
+                        tag: "p",
+                        children: [
+                          "On a imaginé cette plateforme pour rendre l'organisation d'événements simple, rapide et accessible à toutes et tous. Trop souvent, les bonnes idées restent dans un coin de tête, faute d'outils adaptés."
+                        ]
+                      },
+                      {
+                        tag: "p",
+                        children: [
+                          "Avec Meetup Connect, tu as enfin un espace pour transformer tes projets en réalité, sans prise de tête ni barrière technique. Crée, partage, connecte… c'est tout ce qu'on attendait aussi."
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+              tag: "section",
+              attributes: [["class", "banner-femme"]],
+              children: [
+                  {
+                      tag: "img",
+                      attributes: [["src", "../Assets/images/banner-femme.webp"]],
+                  },
+                  {
+                      tag: "div",
+                      attributes: [["class", "banner"]],
+                      children: [
+                          {
+                              tag: "h2",
+                              attributes: [["class", "h2"]],
+                              children: ["Fais vivre tes idées\u00A0: crée ta communauté\u00A0!"],
+                          },
+                          {
+                              tag: "p",
+                              attributes: [["class", "p"]],
+                              children: [
+                                  "Rassemble les bonnes personnes autour de tes passions, idées ou projets. En quelques clics, lance ton propre espace et commence à organiser tes événements",
+                              ],
+                          },
+                          {
+                              tag: "button",
+                              attributes: [["class", "bouton-primary-1"]],
+                              children: ["Je crée ma communauté"],
+                          },
+                      ],
+                  },
+              ],
           }
         ]
       }
