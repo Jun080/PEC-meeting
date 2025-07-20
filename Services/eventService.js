d3ee3531ce07633e38f79bcc5afda43debc19dd9
Ilian Igoudjil <ilianigoudjil@gmail.com>
2025-07-20 22:56:48 +0200

    création de la fonction getAllPopularEvents

---
+++ Services/eventService.js
@@ -0,0 +1,15 @@
+import { getAllEvents } from "../Models/eventModel.js";
+
+export async function getAllPopularEvents() {
+    const events = await getAllEvents();
+    const eventsWithPlacesAchetees = events
+        .filter((event) => event.popular)
+        .map((event) => ({
+            ...event,
+            placesAchetees:
+                (event.nombre_places || 0) -
+                (event.nombre_places_disponibles || 0),
+        }))
+        .sort((a, b) => b.placesAchetees - a.placesAchetees);
+    return eventsWithPlacesAchetees;
+}
